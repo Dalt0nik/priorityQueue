bd1e90d18229e0c92e5170dfe60c1553eb183675
Evgenij Shapovalov <zhenya.shapovalov@gmail.com>
2023-02-28 20:05:20 +0200

    working file system

--- main.c
+++ main.c
@@ -1,28 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "priorityQueue.h"
 
-
-typedef struct node node;
-typedef struct priorityQueue priorityQueue;
-struct node
-{
-    int value;
-    int priority;
-    node* next;
-};
-
-struct priorityQueue
-{
-    node* front;
-};
-
-priorityQueue* create();
-int delete(priorityQueue* pq);
-int isEmpty(priorityQueue* pq);
-int isFull(priorityQueue* pq);
-int insert(priorityQueue* pq, int newValue, int newPriority);
-int pop(priorityQueue* pq, int* poppedValue);
-int peek(priorityQueue* pq, int* topValue, int* topPriority);
 
 void printQ(priorityQueue* pq);
 
@@ -30,7 +9,7 @@ int main()
 {
     priorityQueue* pqList[10] = {NULL};
 
-    pqList[0] = create();
+    pqList[0] = createQ();
     int value, topValue, topPr;
 
     insert(pqList[0], 4, 3);
@@ -45,115 +24,10 @@ int main()
     peek(pqList[0], &topValue, &topPr);
     printf("top v = %d, top pr = %d\n",topValue, topPr);
 
-    delete(pqList[0]);
+    deleteQ(pqList[0]);
     printQ(pqList[0]);
 
     return 0;
-}
-
-priorityQueue* createQ()
-{
-    priorityQueue* newPQ;
-    newPQ = malloc(sizeof(priorityQueue));
-    newPQ->front = NULL;
-    return newPQ;
-}
-
-int isEmpty(priorityQueue* pq)
-{
-    if(pq->front == NULL)
-        return 1;
-    else
-        return 0;
-}
-
-int isFull(priorityQueue* pq)
-{
-    priorityQueue* temp = NULL;
-    temp = malloc(sizeof(priorityQueue));
-    
-    if(temp != NULL)
-    {
-        free(temp);
-        return 0;
-    }  
-    else
-        return 1;  
-}
-
-int insert(priorityQueue* pq, int newValue, int newPriority)
-{
-    if(isFull(pq) == 1)
-        return 0;
-
-
-    if(pq->front == NULL || pq->front->priority > newPriority)
-    {
-        node* temp = pq->front;
-        pq->front = (node*)malloc(sizeof(node));
-        pq->front->next = temp;
-        pq->front->priority = newPriority;
-        pq->front->value = newValue;
-        return 1;
-    }
-
-    node* current;
-    current = pq->front;
-
-    while(current->next != NULL && current->next->priority <= newPriority)
-    {
-        current = current->next;
-    }
-
-    node *newNode = malloc(sizeof(node));
-    newNode->next = current->next;
-    newNode->priority = newPriority;
-    newNode->value = newValue;
-
-    current->next = newNode;
-    return 1;
-}
-
-int pop(priorityQueue* pq, int* poppedValue)
-{
-    if(isEmpty(pq) == 1)
-        return 0;
-
-    node* temp;
-    *poppedValue = pq->front->value;
-    temp = pq->front;
-    pq->front = pq->front->next;
-    free(temp);
-    return 1;
-}
-
-int peek(priorityQueue* pq, int* topValue, int* topPriority)
-{
-    if(isEmpty(pq) == 1)
-        return 0;
-    
-    *topValue = pq->front->value;
-    *topPriority = pq->front->priority;
-}
-
-int deleteQ(priorityQueue* pq)
-{
-    if(isEmpty(pq) == 1)
-        return 0;
-
-    node* current, *prev;
-    
-    current = pq->front;
-
-    while(current != NULL)
-    {
-        prev = current;
-        current = current->next;
-        free(prev);
-    }
-    pq->front = NULL;
-
-    return 1;
 }
 
 void printQ(priorityQueue *pq)
